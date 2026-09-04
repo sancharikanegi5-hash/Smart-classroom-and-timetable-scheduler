990c9ce09119efbe73b910ee98c3b6e2917b1c4d
samiksha-chauhan <chauhansamiksha30@gmail.com>
2026-09-04 11:53:36 +0530

    student-section.h

--- Student-Section/student-section.h
+++ Student-Section/student-section.h
@@ -1,1 +1,40 @@
+#ifndef STUDENT_SECTION_H
+#define STUDENT_SECTION_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define MAX 50
+
+class Student
+{
+private:
+    int rollNo;
+    string name;
+    string section;
+
+public:
+    Student();
+    Student(int r, string n, string s);
+
+    int getRollNo();
+    void display();
+};
+
+class Section
+{
+private:
+    Student list[MAX];
+    int count;
+
+public:
+    Section();
+
+    void addStudent();
+    void removeStudent(int roll);
+    void searchStudent(int roll);
+    void displayAllStudents();
+};
+
+#endif
